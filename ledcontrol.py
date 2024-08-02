033c25c92b0868534be05f2f08fc7d4f9ad6147c
satyendra123 <Singhsatyendra885@gmail.com>
2024-08-02 15:01:58 +0530

    Update ledcontrol.py

--- ledcontrol.py
+++ ledcontrol.py
@@ -1494,29 +1494,19 @@ def index(request):
     return "on"
 app.run(host='0.0.0.0', port=5000, debug=False, ssl=None)
 3) 
-# 导入Microdot
 from lib.microdot import Microdot,send_file
-# 连接wifi
 from common.connect_wifi import do_connect
-# 导入引脚
 from machine import Pin
-# esp32 引脚2是一颗自带的 led的灯
 light = Pin(2,Pin.OUT)
-​
-# 开始连接wifi
 do_connect()
-# 实例化这个类
 app = Microdot()
 ​
-# 返回一个网页
 @app.route('/')
 def index(request):
     return send_file('public/index.html')
 ​
-# 设置一个get请求 如果
 @app.get('/on')
 def index(request):
-    # 如果收到get请求on就开灯
     light.value(1)
     return "开灯了"
 ​
@@ -1526,7 +1516,6 @@ def index(request):
     light.value(0)
     return "关灯了"
 ​
-# 端口号为5000
 app.run(host='0.0.0.0', port=5000, debug=False, ssl=None)
 
 4-a) index.html
